6c2a77dbd5847bfaa8ed88d1569cdf9ce2125c8c
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-04-08 22:31:35 -0700

    setup project environment

--- techreviewproj/settings.py
+++ techreviewproj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'techapp',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,11 @@ WSGI_APPLICATION = 'techreviewproj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'NAME': 'techreviewdb',
+        'USER' : 'postgres',
+        'PASSWORD': 'P@ssw0rd1',
+        'HOST' :'localhost',
+        'PORT' :'',
     }
 }
 
